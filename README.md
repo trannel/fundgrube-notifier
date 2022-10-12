9a2fe30c664bd5752120d407ce8b0494ad6c0e56
Lennart Kuell <lennart.kuell@gmail.com>
2022-10-12 21:57:42 +0200

    Add check for specific prices

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# fundgrube_notifier
+# Fundgrube notifier
+
+This project notifies you about current offers from the German electronics retail chains Saturn and Media Markt in their
+Fundgrube, that match specific conditions you can define.
+
+## Usage
+
+First, install the project and the required packages from the `requirements.txt`.
+Next, create a file called `products.json` in the root directory of the project, and once it is filled, run
+the `main.py`.
+Entries in the file are created in JSON format and every JSON object can have the following attributes:
+
+- terms: A list of terms that must be included in the name of the article (case-insensitive). Mandatory attribute.
+- price: You will not get a notification, if this price is surpassed. Optional attribute.
+
+Example:
+
+```json
+[
+  {
+    "terms": ["sony", "tv"]
+  },
+  {
+    "terms": ["playstation"],
+    "price": 20
+  }
+]
+```
+
+You will get notification for every article with the terms "sony" and "tv" in the name regardless of its price and every
+product with "playstation" that is 20€ or less.
+
+## Fundgrube
+
+The Fundgrube has special offers that are usually very limited in numbers and can be found here
+
+- Saturn: https://www.saturn.de/de/data/fundgrube
+- Media Markt: https://www.mediamarkt.de/de/data/fundgrube
+
+### Data
+
+The project is using already preprocessed data, which is crawled hourly by a script
+from [Barney](https://www.mydealz.de/profile/Barney) at [mydealz](https://www.mydealz.de/):
+
+- Saturn: https://schneinet.de/saturn.html
+- Media Markt: https://schneinet.de/mediamarkt.html
+
+More info:
+https://www.mydealz.de/diskussion/die-saturn-fundgrube-ist-da-einzelstucke-in-einzelnen-markten-gunstig-kaufen-1764598
+ 
--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from datetime import datetime
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from requests.adapters import HTTPAdapter, Retry
 
 log.basicConfig(level=log.DEBUG)
 
-chains = [
+retailers = [
     {
         "name": "Saturn",
         "url": "https://schneinet.de/saturn.html"
@@ -19,58 +20,63 @@ chains = [
         "url": "https://schneinet.de/mediamarkt.html"
     }]
 
+s = requests.Session()
+retries = Retry(total=3, backoff_factor=1, status_forcelist=[502, 503, 504])
+adapter = HTTPAdapter(max_retries=retries)
+s.mount('http://', adapter)
+s.mount('https://', adapter)
 
-def request(chain: chains) -> str:
-    filename_html = f"{chain['name']}.html"
 
-    if os.path.isfile(filename_html):
+def request(retailer: retailers) -> str:
+    filename_html = f"{retailer['name']}.html"
+
+    prod = os.environ.get("ENV") == 'prod'
+
+    if not prod and os.path.isfile(filename_html):
         timestamp = os.path.getmtime(filename_html)
         time_diff = datetime.now().timestamp() - timestamp
         if time_diff < 50 * 60:
             with open(filename_html, "r", encoding="utf-8") as file:
                 log.debug("Loaded items from file")
                 return file.read()
-    html = requests.get(chain["url"]).content
-    with open(filename_html, "wb") as file:
-        file.write(html)
+    html = requests.get(retailer["url"]).content
+    if not prod:
+        with open(filename_html, "wb") as file:
+            file.write(html)
     return html.decode("utf-8")
-
-    # print(response.status_code)
-    # print(response.content)
-    # TODO error handling
 
 
 def create_new_items() -> pd.DataFrame:
-    with open("terms.json", "r", encoding="utf-8") as search_file:
-        terms = json.load(search_file)
+    with open("products.json", "r", encoding="utf-8") as search_file:
+        products = json.load(search_file)
     df = pd.DataFrame({})
 
-    for chain in chains:
-        log.info(f"Request {chain['name']} data")
-        html = request(chain)
-        log.info("Create soup")
+    for retailer in retailers:
+        log.debug(f"Request {retailer['name']} data")
+        html = request(retailer)
+        log.debug("Create soup")
         soup = BeautifulSoup(html, 'html.parser')
-        log.info("Parse soup")
+        log.debug("Parse soup")
         body = soup.body
 
-        for term_obj in terms:
-            tags = body.find_all('a', text=lambda text: text and all(
-                [term in text.lower() for term in term_obj["terms"]]))
-            stores = [tag.find_parent("div").previous_sibling.contents[0].text.strip() for tag in
-                      tags]
+        for product in products:
+            terms = [x.lower() for x in product["terms"]]
+            tags = body.find_all('a', text=lambda text: text and all([term in text.lower() for term in terms]))
+            stores = [tag.find_parent("div").previous_sibling.contents[0].text.strip() for tag in tags]
             images = [tag.get("href") for tag in tags]
             names = [tag.text for tag in tags]
             prices = [tag.parent.previous_sibling.contents[0].text for tag in tags]
-            df_tmp = pd.DataFrame(
-                {"name": names, "price": prices, "store": stores, "image": images})
+            df_tmp = pd.DataFrame({"name": names, "price": prices, "store": stores, "image": images})
             if df_tmp.size > 0:
-                df_tmp["store"] = chain["name"] + " - " + df_tmp["store"]
-            df = pd.concat([df, df_tmp])
+                if "price" in product:
+                    df_tmp = df_tmp[pd.to_numeric(df_tmp["price"].str.slice(stop=-4)) <= product.get("price")]
+                df_tmp["store"] = retailer["name"] + " - " + df_tmp["store"]
+                df = pd.concat([df, df_tmp])
     return df
 
 
 def load_old_items(filename: str) -> pd.DataFrame:
-    log.info("Load previous results")
+    log.debug("Load previous results")
     if os.path.isfile(filename):
         return pd.read_csv(filename, header=0, encoding="utf-8",
                            dtype={"name": "object", "price": "object", "store": "object",
@@ -84,10 +90,8 @@ def process_dfs(df_new: pd.DataFrame, df_old: pd.DataFrame):
     new_count = df["time"].isna().sum()
 
     df["time"] = df["time"].fillna(datetime.now())
-    # TODO better notifications
-    df = df.sort_values(by=["time"], ascending=False)
-
-    log.info(f"There were {new_count} new results")
+    df = df.sort_values(by=["time", "store", "name"], ascending=False)
+    log.info(f"There were {new_count} new results")  # TODO better notifications
     if new_count > 0:
         pd.set_option('display.max_rows', None)
         pd.set_option('display.max_columns', None)
@@ -95,13 +99,13 @@ def process_dfs(df_new: pd.DataFrame, df_old: pd.DataFrame):
         pd.set_option('display.max_colwidth', None)
         log.info(df[0:new_count])
 
-    log.info("Save results")
-    df.to_csv(filename, encoding="utf-8", index=False)
+        log.debug("Save results")
+        df.to_csv(filename, encoding="utf-8", index=False)
 
 
 if __name__ == '__main__':
-    log.info("Start script")
-    filename = "previous.csv"
+    log.debug("Start script")
+    filename = "results.csv"
 
     df_new = create_new_items()
     df_old = load_old_items(filename)
